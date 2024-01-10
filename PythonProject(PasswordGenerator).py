c8676520a51eb244de4ea43fbe2b4bf2be8e9e5f
SouravD26 <156168011+SouravD26@users.noreply.github.com>
2024-01-11 00:46:59 +0530

    Add files via upload

---
+++ PythonProject(PasswordGenerator).py
@@ -0,0 +1,24 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      DELL PC
+#
+# Created:     02-12-2023
+# Copyright:   (c) DELL PC 2023
+# Licence:     <your licence>
+#------------------------------------------------------------------------------
+# password Generator
+
+# Import random module
+import random
+
+# Input statement
+password_len = int(input("Enter Password length. "))
+
+# Symbol & Character for Generate Password
+password_data = "QWERTYUIOPASDFGHJKLZXCVBNM1234567890@$&%"
+
+Password = "".join(random.sample(password_data,password_len))
+
+print(f"Your Password is {Password} ")
